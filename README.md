d405cc86caef2dfdfefb03f39e7671ec00bf59ff
Daira Hopwood <daira@jacaranda.org>
2019-01-03 17:04:38 +0000

    Add cumulative frequency graph for confirmation times.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Dual PoW simulations
 
 Undocumented Python 3 programs for simulating dual PoW and confirmation times for Zcash.
 [MIT licensed](LICENSE).
+
+confirmationtime.py has a dependency on Numpy and Matplotlib.
--- confirmationtime.py
+++ confirmationtime.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 from numpy.random import exponential
-from numpy import mean, median, percentile
+from numpy import mean, median, percentile, histogram, cumsum
+from matplotlib import pyplot
 
 NSAMPLES = 1000000
 
-def simulate(expectation, confirmations):
-    print(expectation, confirmations)
+def simulate(expectation, confirmations, colour):
+    print(expectation, confirmations, colour)
     times = [sum(exponential(expectation, confirmations)) for i in range(NSAMPLES)]
 
     experimental_mean   = mean(times)
@@ -18,6 +19,18 @@ def simulate(expectation, confirmations):
     print(experimental_99pc)
     print()
 
-simulate(150, 10)
-simulate(75, 21)
+    # <https://stackoverflow.com/a/15419072/393146>
+    # evaluate the histogram
+    values, base = histogram(times, bins=100)
+    # evaluate the cumulative
+    cumulative = cumsum(values)*100/NSAMPLES
+    # plot the cumulative function
+    pyplot.plot(base[:-1], cumulative, c=colour, label="%d %d" % (expectation, confirmations))
+
+simulate(150, 10, 'blue')
+simulate(75, 21, 'green')
+
+pyplot.title("Confirmation time simulation")
+pyplot.legend()
+pyplot.show()
 
