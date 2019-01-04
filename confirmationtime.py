2033965c1d858aa17cb3d90e539970ae34d025c7
Daira Hopwood <daira@jacaranda.org>
2019-01-04 08:15:06 +0000

    Plot both cumulative frequency and (normalized) frequency graphs.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- confirmationtime.py
+++ confirmationtime.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 from numpy.random import exponential
-from numpy import mean, median, percentile, histogram, cumsum
+from numpy import mean, median, percentile, histogram, cumsum, max
 from matplotlib import pyplot
 
 NSAMPLES = 1000000
 
-def simulate(expectation, confirmations, colour):
-    print(expectation, confirmations, colour)
+def simulate(expectation, confirmations):
+    print(expectation, confirmations)
     times = [sum(exponential(expectation, confirmations)) for i in range(NSAMPLES)]
 
     experimental_mean   = mean(times)
@@ -18,19 +18,34 @@ def simulate(expectation, confirmations, colour):
     print(experimental_median)
     print(experimental_99pc)
     print()
+    return times
 
+def cumulative_frequency(times, label, colour):
     # <https://stackoverflow.com/a/15419072/393146>
     # evaluate the histogram
     values, base = histogram(times, bins=100)
     # evaluate the cumulative
     cumulative = cumsum(values)*100/NSAMPLES
     # plot the cumulative function
-    pyplot.plot(base[:-1], cumulative, c=colour, label="%d %d" % (expectation, confirmations))
+    pyplot.plot(base[:-1], cumulative, c=colour, label=label)
 
-simulate(150, 10, 'blue')
-simulate(75, 21, 'green')
+def frequency(times, label, colour):
+    values, base = histogram(times, bins=100)
+    pyplot.plot(base[:-1], values/max(values), c=colour, label=label)
 
-pyplot.title("Confirmation time simulation")
-pyplot.legend()
-pyplot.show()
 
+results = [
+    (simulate(150, 10), 150, 10, 'blue'),
+    (simulate( 75, 21),  75, 21, 'green'),
+]
+
+def show_graph(plotfn, subtitle):
+    for (times, expectation, confirmations, colour) in results:
+        plotfn(times, "%ds blocks, %d confirmations" % (expectation, confirmations), colour)
+
+    pyplot.title("Confirmation time simulation (%s)" % (subtitle,))
+    pyplot.legend()
+    pyplot.show()
+
+show_graph(cumulative_frequency, "cumulative frequency")
+show_graph(frequency, "frequency, normalized")
