4600c7c4a6eba4fe5ac81f66e7765e4d83da7a5f
Daira Hopwood <daira@jacaranda.org>
2019-01-06 20:10:52 +0000

    Better attacker strategy for difficulty manipulation: try making each algorithm harder and see which works better :-) (it varies in a nonobvious way).
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- attacksim.py
+++ attacksim.py
@@ -29,22 +29,25 @@ def simulate_parallel(manipulation_advantage, adv_share_A, adv_share_B):
     debug("adv_share_A             =", adv_share_A)
     debug("adv_share_B             =", adv_share_B)
 
-    blocktime_A = BLOCKTIME*manipulation_advantage
-    blocktime_B = BLOCKTIME
-    honest_share_A = 1.0-adv_share_A
-    honest_share_B = 1.0-adv_share_B
-    honest_blocktimes = [blocktime_A/honest_share_A, blocktime_B/honest_share_B]
-    adv_blocktimes    = [blocktime_A/adv_share_A,    blocktime_B/adv_share_B]
+    adv_successes = deque()
+    for (blocktime_A, blocktime_B) in [(BLOCKTIME*manipulation_advantage, BLOCKTIME),
+                                       (BLOCKTIME, BLOCKTIME*manipulation_advantage)]:
+        honest_share_A = 1.0-adv_share_A
+        honest_share_B = 1.0-adv_share_B
+        honest_blocktimes = [blocktime_A/honest_share_A, blocktime_B/honest_share_B]
+        adv_blocktimes    = [blocktime_A/adv_share_A,    blocktime_B/adv_share_B]
 
-    adv_wins = 0
-    for i in range(NTRIALS):
-        honest_time = parallel_trial(honest_blocktimes, CONFIRMATIONS)
-        adv_time    = parallel_trial(adv_blocktimes,    CONFIRMATIONS)
+        adv_wins = 0
+        for i in range(NTRIALS):
+            honest_time = parallel_trial(honest_blocktimes, CONFIRMATIONS)
+            adv_time    = parallel_trial(adv_blocktimes,    CONFIRMATIONS)
 
-        if adv_time < honest_time:
-            adv_wins += 1
+            if adv_time < honest_time:
+                adv_wins += 1
 
-    adv_success = adv_wins/NTRIALS
+        adv_successes.append(adv_wins/NTRIALS)
+
+    adv_success = max(adv_successes)
     debug("parallel PoW success    =", adv_success)
     debug()
     return adv_success
@@ -75,26 +78,29 @@ def simulate_alternating(manipulation_advantage, adv_share_A, adv_share_B):
     debug("adv_share_A             =", adv_share_A)
     debug("adv_share_B             =", adv_share_B)
 
-    blocktime_A = BLOCKTIME/2.0
-    blocktime_B = (BLOCKTIME/2.0)*manipulation_advantage
-    honest_share_A = 1.0-adv_share_A
-    honest_share_B = 1.0-adv_share_B
-    honest_blocktime_A = blocktime_A/honest_share_A
-    honest_blocktime_B = blocktime_B/honest_share_B
-    adv_blocktime_A = blocktime_A/adv_share_A
-    adv_blocktime_B = blocktime_B/adv_share_B
+    adv_successes = deque()
+    for (blocktime_A, blocktime_B) in [(BLOCKTIME/2.0*manipulation_advantage, BLOCKTIME/2.0),
+                                       (BLOCKTIME/2.0, BLOCKTIME/2.0*manipulation_advantage)]:
+        honest_share_A = 1.0-adv_share_A
+        honest_share_B = 1.0-adv_share_B
+        honest_blocktime_A = blocktime_A/honest_share_A
+        honest_blocktime_B = blocktime_B/honest_share_B
+        adv_blocktime_A = blocktime_A/adv_share_A
+        adv_blocktime_B = blocktime_B/adv_share_B
 
-    adv_wins = 0
-    for i in range(NTRIALS):
-        honest_time = (sum(exponential(honest_blocktime_A, ALTERNATING_CONFIRMATIONS)) +
-                       sum(exponential(honest_blocktime_B, ALTERNATING_CONFIRMATIONS)))
-        adv_time    = (sum(exponential(adv_blocktime_A, ALTERNATING_CONFIRMATIONS)) +
-                       sum(exponential(adv_blocktime_B, ALTERNATING_CONFIRMATIONS)))
+        adv_wins = 0
+        for i in range(NTRIALS):
+            honest_time = (sum(exponential(honest_blocktime_A, ALTERNATING_CONFIRMATIONS)) +
+                           sum(exponential(honest_blocktime_B, ALTERNATING_CONFIRMATIONS)))
+            adv_time    = (sum(exponential(adv_blocktime_A, ALTERNATING_CONFIRMATIONS)) +
+                           sum(exponential(adv_blocktime_B, ALTERNATING_CONFIRMATIONS)))
 
-        if adv_time < honest_time:
-            adv_wins += 1
+            if adv_time < honest_time:
+                adv_wins += 1
 
-    adv_success = adv_wins/NTRIALS
+        adv_successes.append(adv_wins/NTRIALS)
+
+    adv_success = max(adv_successes)
     debug("alternating PoW success =", adv_success)
     debug()
     return adv_success
